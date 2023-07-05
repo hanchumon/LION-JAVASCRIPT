b5779f355db36c393c5658cc3201a5eb6f284184
hanchumon <leehyj1112@gmail.com>
2023-07-05 11:20:10 +0900

    05.datatype학습

---
+++ client/chapter/core/02.variable.js
@@ -0,0 +1,40 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin, name;
+name = 'John';
+admin = name;
+console.log(admin)
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceQuantity;
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+// - 구매 결제 여부
+let isPayment;
+// - 구매 결제 내역
+let paymentHistory;
+// - 브랜드 접두사
+const BRAND_PREFIX='NIKE'
+// - 오늘의 운세
+let todayHoroscope;
+// - 상품 정보
+const productInfo=null;
+
+
+/* variables ----------------------------------------------------------- */
+
+
+/* constant variables -------------------------------------------------- */
+
+// panding 아무것도 안함
+// loading 로딩중, 요청대기
+// fulfilled, resolved 응답성공
+// reject 실패
+
